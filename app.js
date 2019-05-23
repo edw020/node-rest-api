22bd821a0e8f29e5b26eb4ae18f90d8e3244f6c8
Victor Edward Calle <victor@bybsoft.com>
2019-05-22 19:30:11 -0600

    Created logic for signing up users, also and endpoint for login user, which validates and then generates a jwt (json web token) to send on response.

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const mongoose = require('mongoose');
 const multer = require('multer');
 
 const feedRoutes = require('./routes/feed');
+const authRoutes = require('./routes/auth');
 
 const app = express();
 
@@ -40,14 +41,16 @@ app.use((req, res, next) => {
 });
 
 app.use('/feed', feedRoutes);
+app.use('/auth', authRoutes);
 
 // Error handlind middleware
 app.use((error, req, res, next) => {
     console.log(error);
     const status = error.statusCode || 500;
     const message = error.message;
+    const data = error.data;
 
-    res.status(status).json({message: message});
+    res.status(status).json({message: message, data: data});
 });
 
 
