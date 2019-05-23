c874d46861e393a251411c48755ea35d500c134d
Victor Edward Calle <victor@bybsoft.com>
2019-05-22 19:48:41 -0600

    Adding an isAuthenticated middleware function which will verify the jwt to be valid and let continue the process (for feed endpoints) only for logged in users.

---
+++ middleware/is-auth.js
@@ -0,0 +1,27 @@
+const jwt = require('jsonwebtoken');
+
+module.exports = (req, res, next) => {
+    const authHeader = req.get('Authorization');
+    if(!authHeader){
+        const error = new Error('Not authenticated.');
+        error.statusCode = 401;
+        throw error;
+    }
+    const token = authHeader.split(' ')[1];
+    let decodeToken;
+    try{
+        decodeToken = jwt.verify(token, 'somesupersecretkey');
+    } catch (err) {
+        err.statusCode = 500;
+        throw err;
+    }
+
+    if(!decodeToken){
+        const error = new Error('Not authenticated.');
+        error.statusCode = 401;
+        throw error;
+    }
+
+    res.userId = decodeToken.userId;
+    next();
+};
