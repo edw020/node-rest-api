bc03210fdd3127bc54458e15e682e2b771d27509
Victor Edward Calle <victor@bybsoft.com>
2019-05-27 20:08:58 -0600

    Implementing GraphQL so now all the api routes created get replaced by a single graphQL api which will work with Schemas and Resolvers to provide the previously existing functionality.

--- app.js
+++ app.js
@@ -4,9 +4,12 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const multer = require('multer');
+const graphqlHttp = require('express-graphql');
 
-const feedRoutes = require('./routes/feed');
-const authRoutes = require('./routes/auth');
+const graphqlSchema = require('./graphql/schema');
+const graphqlResolver = require('./graphql/resolvers');
+const auth = require('./middleware/auth');
+const { clearImage } = require('./util/file');
 
 const app = express();
 
@@ -37,11 +40,52 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*'); // Allowing call from any domain
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE'); // Allows these methods on the requests
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization'); // Only allows this header types on requests
+
+    // This is for fixing CORS error on graphql requests
+    if(req.method === 'OPTIONS')
+        return res.sendStatus(200);
+
     next();
 });
 
-app.use('/feed', feedRoutes);
-app.use('/auth', authRoutes);
+app.use(auth);
+
+app.put('/post-image', (req, res, next) => {
+    if(!req.isAuth){
+        throw new Error('Not authenticated!');
+    }
+
+    if(!req.file){
+        return res.status(200).json({message: 'No file provided!'});
+    }
+
+    if(req.body.oldPath){
+        clearImage(req.body.oldPath);
+    }
+
+    return res.status(201).json({message: 'File stored', filePath: req.file.path});
+});
+
+app.use('/graphql', graphqlHttp({
+    schema: graphqlSchema,
+    rootValue: graphqlResolver,
+    graphiql: true,
+    formatError(err){
+        if(!err.originalError){
+            return err;
+        }
+
+        const data = err.originalError.data;
+        const message = err.message || 'An error ocurred';
+        const code = err.originalError.code || 500;
+
+        return {
+            message,
+            status: code,
+            data
+        }
+    }
+}));
 
 // Error handlind middleware
 app.use((error, req, res, next) => {
@@ -56,10 +100,6 @@ app.use((error, req, res, next) => {
 
 mongoose.connect('mongodb+srv://edward:P4ssw0rd2o19@cluster0-hehis.mongodb.net/messages?retryWrites=true')
     .then(res => {
-        const server = app.listen(8080);
-        const io = require('./socket').init(server);
-        io.on('connection', socket => {
-            console.log('Client connected');
-        });
+        app.listen(8080);
     })
     .catch(err => console.log(err));
